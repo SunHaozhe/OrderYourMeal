1bbece4cdc654fb73d32a02876f1c5b18f932fd0
Sun-Haozhe <275940200@QQ.COM>
2018-04-07 10:16:16 +0200

    Final version (optimization of layout of filter.ui)

--- filter.cpp
+++ filter.cpp
@@ -6,24 +6,11 @@ Filter::Filter(QWidget *parent) :
     ui(new Ui::Filter)
 {
 
-
     ui->setupUi(this);
     setWindowTitle("Filter");
-    //mainwidget and mainlayout
     QWidget * mainwidget = new QWidget;
     mainLayout = new QVBoxLayout;
-
-    ////welcome
-    QHBoxLayout * Layout0 = new QHBoxLayout;
     QLabel * label0 = new QLabel("Vous pouvez choisir ce que vous ne mangez pas ici");
-    Layout0 ->addWidget(label0);
-
-    //add dislike ingredients
-    QVBoxLayout * Layout1 = new QVBoxLayout;
-    QHBoxLayout * Layout11 = new QHBoxLayout;
-    QHBoxLayout * Layout12 = new QHBoxLayout;
-
-
     QLabel * label1 = new QLabel("Ingrédients:");
     ing1 = new QCheckBox("Boeuf", this);
     ing2 = new QCheckBox("legume", this);
@@ -33,24 +20,34 @@ Filter::Filter(QWidget *parent) :
     ing6 = new QCheckBox("poisson", this);
     ing7 = new QCheckBox("porc", this);
     ing8 = new QCheckBox("poulet", this);
-    QWidget *empty1 = new QWidget;
-    empty1->setFixedWidth(20);
 
-    Layout11 ->addWidget(label1);
-    Layout11 ->addWidget(ing1);
-    Layout11 ->addWidget(ing2);
-    Layout11 ->addWidget(ing3);
-    Layout11 ->addWidget(ing4);
-    Layout11 ->addWidget(ing5);
+    QVBoxLayout * Layout1 = new QVBoxLayout;
+    QHBoxLayout * Layout2 = new QHBoxLayout;
 
-    Layout12 ->addWidget(empty1);
-    Layout12 ->addWidget(ing6);
-    Layout12 ->addWidget(ing7);
-    Layout12 ->addWidget(ing8);
+    QVBoxLayout * Layout15 = new QVBoxLayout;
+    QVBoxLayout * Layout26 = new QVBoxLayout;
+    QVBoxLayout * Layout37 = new QVBoxLayout;
+    QVBoxLayout * Layout48 = new QVBoxLayout;
 
-    Layout1 ->addLayout(Layout11);
-    Layout1 ->addLayout(Layout12);
-    /*
+    Layout15 ->addWidget(ing1);
+    Layout15 ->addWidget(ing5);
+    Layout26 ->addWidget(ing2);
+    Layout26 ->addWidget(ing6);
+    Layout37 ->addWidget(ing3);
+    Layout37 ->addWidget(ing7);
+    Layout48 ->addWidget(ing4);
+    Layout48 ->addWidget(ing8);
+
+    Layout2 -> addLayout(Layout15);
+    Layout2 -> addLayout(Layout26);
+    Layout2 -> addLayout(Layout37);
+    Layout2 -> addLayout(Layout48);
+
+    Layout1 ->addWidget(label0);
+    Layout1 ->addWidget(label1);
+    Layout1 ->addLayout(Layout2);
+
+/*
     QHBoxLayout * Layout2 = new QHBoxLayout;
     QLabel * label2 = new QLabel("Saveurs: ");
     QCheckBox *saveur1 = new QCheckBox("Salé", this);
@@ -74,19 +71,17 @@ Filter::Filter(QWidget *parent) :
     Layout3->addWidget(empty2);
     Layout3->addWidget(quit);
 
-    choicesWidget = new QWidget;
-    QVBoxLayout* choicesLayout = new QVBoxLayout;
-    choicesLayout->addLayout(Layout0);
-    choicesLayout->addLayout(Layout1);
-    //choicesLayout->addLayout(Layout2);
-    choicesWidget->setLayout(choicesLayout);
+   choicesWidget = new QWidget;
+   QVBoxLayout* choicesLayout = new QVBoxLayout;
+   choicesLayout->addLayout(Layout1);
+   choicesWidget->setLayout(choicesLayout);
 
-    //add to mainlayout
+   //add to mainlayout
     mainLayout->addWidget(choicesWidget);
     mainLayout->addLayout(Layout3);
 
-    this->setLayout(mainLayout);
-    mainwidget->setFixedSize(350, 200);
+   this->setLayout(mainLayout);
+   mainwidget->setFixedSize(350, 200);
 
 
 }
@@ -102,54 +97,54 @@ void Filter::confirmSLOT()
     ingrediantName.clear();
     if(ing1->isChecked())
     {
-        ingrediantName.push_back( "boeuf");
+     ingrediantName.push_back( "boeuf");
 
 
     }
     if(ing2->isChecked())
     {
-        ingrediantName.push_back( "legume");
+     ingrediantName.push_back( "legume");
 
 
     }
     if(ing3->isChecked())
     {
-        ingrediantName.push_back( "fruit_de_mer");
+     ingrediantName.push_back( "fruit_de_mer");
 
 
     }
     if(ing4->isChecked())
     {
-        ingrediantName.push_back("pomme_de_terre");
-
+     ingrediantName.push_back("pomme_de_terre");
 
     }
 
     if(ing5->isChecked())
     {
-        ingrediantName.push_back("pate");
+     ingrediantName.push_back("pate");
 
 
     }
     if(ing6->isChecked())
     {
-        ingrediantName.push_back("poisson");
+     ingrediantName.push_back("poisson");
 
 
     }
     if(ing7->isChecked())
     {
-        ingrediantName.push_back("porc");
+     ingrediantName.push_back("porc");
 
 
     }
     if(ing8->isChecked())
     {
-        ingrediantName.push_back("poulet");
+     ingrediantName.push_back("poulet");
+
 
     }
-    emit confirm();
-    //cout<<"label    "<<label<<endl;
-    this->hide();
+      emit confirm();
+     cout<<"label    "<<label<<endl;
+     this->hide();
 
 }
